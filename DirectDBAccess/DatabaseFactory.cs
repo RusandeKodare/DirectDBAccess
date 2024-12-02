20d6f6d2f57b4af324087c4fc9ce6b45fdf23d98
RusandeKodare <o.p.j@hotmail.com>
2024-12-02 23:09:14 +0100

    feat: add DatabaseFactory with ChooseDb method
    - Implement DatabaseFactory class.
    - Added the ChooseDb method, which uses a switch expression to create and return an instance of the appropriate database class.

---
+++ DirectDBAccess/DatabaseFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectDBAccess
+{
+    public class DatabaseFactory
+    {
+        private readonly string _connectionString;
+
+        public DatabaseFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public IDatabase ChooseDb(int dbChoice)
+        {
+            return dbChoice switch
+            {
+                1 => new SqlServer(_connectionString),
+                2 => new MongoDb(_connectionString),
+                _ => throw new ArgumentException("Invalid choice. Please select a valid database type.")
+            };
+        }
+
+
+    }
+}
